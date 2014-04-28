37f34ac5292ca592f11fef5866c4ab57d619a732
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 16:07:03 +1000

    function to make summary table is copied from WTC_IEM

--- R/functions.R
+++ R/functions.R
@@ -96,3 +96,34 @@ cmbn.fls <- function(file){
   pr.cst <- pr.cst[order(pr.cst$Incubation, pr.cst$Date, as.numeric(pr.cst$Chamber), as.numeric(pr.cst$Side)),]
   return(pr.cst)
 }
+
+##########################
+# Create a summary table #
+##########################
+CreateTable <- function(dataset, fac){
+  #dataset=iem for chamber summary, ChMean for temp trt summary, fac = Chamber or temp, nutrient=no/nh/po
+  a <- dataset[c("date", fac, "value")] #extract required columns
+  colnames(a) <- c("date","variable","value") #change column names for cast
+  means <- cast(a, date~variable, mean, na.rm = TRUE) 
+  ses <- cast(a,date~variable,function(x) ci(x,na.rm=TRUE)[4])
+  colnames(ses)[2:ncol(ses)] <- paste(colnames(ses)[2:ncol(ses)],"SE",sep=".")
+  samples <- cast(a,date~variable,function(x) sum(!is.na(x))) #sample size
+  colnames(samples)[2:ncol(samples)] <- paste(colnames(samples)[2:ncol(samples)],"N",sep=".")
+  mer <- Reduce(function(...) merge(..., by = "date"), list(means, ses, samples)) #merge datasets
+  mer <- mer[,c(1, order(names(mer)[-grep("date|N", names(mer))])+1, grep("N", names(mer)))] #re-order columns
+  mer$date <- as.character(mer$date) # date is turned into character for knitr output 
+  return(mer)
+}
+
+#function which creates excel worksheets
+crSheet <- function(sheetname, dataset){
+  #create sheet
+  sheet <- createSheet(wb, sheetName = sheetname)
+  
+  #add data to the sheet
+  addDataFrame(dataset, sheet, showNA = TRUE, row.names = FALSE, startRow = 2)
+  
+  #title of the sheet
+  addDataFrame(t(c(sheetname, "unit=ug cm^(-2) day^(-1))")), sheet, startRow = 1, row.names = FALSE, col.names = FALSE)
+}
+
