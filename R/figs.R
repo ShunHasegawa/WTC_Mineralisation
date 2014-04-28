5fe22802bdf4406435b75a3e09c01fa1e2777cad
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 19:16:22 +1000

    plot figs and save

--- R/figs.R
+++ R/figs.R
@@ -1,4 +1,18 @@
 # summary data frame
 ChMean <- ddply(mineMlt, .(time, date, insertion, sampling, temp, chamber, variable), Crt_SmryDF) 
-TrtMean <- TrtMean <- ddply(ChMean, .(Time, insertion, sampling, date, temp, variable), 
+TrtMean <- TrtMean <- ddply(ChMean, .(time, insertion, sampling, date, temp, variable), 
                             function(x) Crt_SmryDF(x, val = "Mean"))
+
+palette(c("blue2", "goldenrod1", "firebrick2", "chartreuse4", "deepskyblue1", "darkorange1", 
+          "darkorchid3", "darkgrey", "mediumpurple1", "orangered2", "chocolate", "burlywood3"))
+
+theme_set(theme_bw()) # graphic backgroud is white
+
+# plot each nutrient separately
+ChFg <- dlply(ChMean, .(variable), PltChmMean)
+fls <- paste("Output/Figs/WTC_Mineralisation_Chamber_", c("Nitrification", "N_mineralisation", "P_mineralisation"), ".pdf",sep = "")
+l_ply(1:3, function(x) ggsave(filename = fls[x], plot = ChFg[[x]], width = 6, height = 3))
+
+TrtFg <- dlply(TrtMean, .(variable), PltTmpMean)
+fls <- paste("Output/Figs/WTC_Mineralisation_Temp_", c("Nitrification", "N_mineralisation", "P_mineralisation"), ".pdf",sep = "")
+lapply(1:3, function(x) ggsave(filename = fls[x], plot = TrtFg[[x]], width = 6, height = 3))
