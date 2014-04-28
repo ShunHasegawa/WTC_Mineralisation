21ed08c172c5cb11d5ff0faf4e4fd6a9422ac2da
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 14:37:51 +1000

    negative values in aq2 resutlts are fixed but it's not accurate becuase it sould be calculate from Absorbance rather than Results. but it shouldn't affect the final analysis too much case it's very small

---
+++ R/functions.R
@@ -0,0 +1,92 @@
+###################################
+# Coorect AQ2 result based on CCV #
+###################################
+
+# subset dataset beween each ccv
+Crrct.ccv <- function(x, data, ccval = 7.5){
+  # subset dataset between given ccvs
+  ccv.ps <- grep("V$", as.character(data$Sample.ID))
+  
+  ts <- data[c(ccv.ps[x] : ccv.ps[x + 1]),]
+  trng <- range(ts$times)
+  xv <- as.numeric(trng)
+  yv <- ts$Result[ts$times == trng[1] | ts$times == trng[2]] 
+  
+  b <- ccval * (1 / yv[1] - 1 / yv[2]) / (xv[1] - xv[2])
+  a <- ccval / yv[1] - b * xv[1]
+  
+  ts$Result <- ts$Result * (a + b * as.numeric(ts$times))
+  ts$times <- NULL
+  return(ts)
+}
+
+# applied the function above for each subsets of ccvs
+Crrtct.ccv.df <- function(filename, ccval = 7.5){
+  data <- read.csv(paste("Data/AQ2/NeedToBeCorrected/", filename, sep = ""), header = TRUE)
+  
+  # make time factor as numeric
+  a <- sapply(as.character(data$Time), strsplit, " ")
+  b <- ldply(a, function(x) paste(x[c(5, 2, 3, 4)], collapse = "/"))
+  
+  b$V1 <- ymd_hms(b$V1)
+  
+  names(b) <- c("Time", "times")
+  
+  # merge
+  mrg.res <- merge(data, b, by = "Time")
+  
+  # reorder accoding to time
+  mrg.res <- mrg.res[order(mrg.res$times), ]
+  
+  # add the latest ccv value at the end of data frame to make sure all measured values are placed between ccvs
+  # if the last line is not ccv, vlues after last ccv will be turned into NA
+  ccv.ps <- grep("V$", as.character(mrg.res$Sample.ID))
+  lstTime <- mrg.res$times[nrow(mrg.res)]
+  mrg.res[nrow(mrg.res) + 1, ] <- mrg.res[max(ccv.ps), ] 
+  mrg.res$times[nrow(mrg.res)] <- lstTime + 1 # make the last time latest by adding 1 to the actual latest time
+  
+  ccv.ps <- grep("V$", as.character(mrg.res$Sample.ID)) # update ccv.position
+  
+  # re-caluculate the results
+  a <- ldply(1:(length(ccv.ps)-1), function(x) Crrct.ccv(x, data = mrg.res, ccval))
+  
+  # if there are negative values, add minimum value to remove any negative values
+  if(any(a$Result < 0)) a$Result <- a$Result - min(a$Result, na.rm = TRUE)
+  
+  return(a)
+}
+
+######################################################
+# process and combine aq 2 data, then create a table #
+######################################################
+prcsAQ2 <- function(data){
+  # remove ccv, ccb, standard
+  res <- data[-grep("^C|^STANDARD", as.character(data$Sample.ID)),]
+  
+  # remove dup, top, middle
+  res <- res[-grep("dup$|top|middle", as.character(res$Sample.Details)),]
+  
+  # sample labels
+  a <- strsplit(as.character(res$Sample.Details), "[.]")
+  
+  # turn this into data frame
+  a.df <- ldply(a)
+  names(a.df)[c(1, 4, 5)] <- c("Date", "ring", "plot")
+  a.df$Date <- ymd(a.df$Date)
+  res.df <- cbind(a.df, res)
+  res.df <- res.df[c("Date", "ring", "plot", "Result")]
+  return(res.df)
+}
+
+cmbn.fls <- function(file){
+  # read files
+  rd.fls <- lapply(file, function(x) read.csv(paste("Data/AQ2/ReadyToProcess/", x, sep = ""), header = TRUE))
+  
+  # process and make data frame for each test type
+  pr.df <- ldply(rd.fls, function(x) ddply(x, .(Test.Name), prcsAQ2))
+  
+  # reshape
+  names(pr.df)[5] <- "value"
+  pr.cst <- cast(pr.df, Date + ring + plot ~ Test.Name)
+  return(pr.cst)
+}
