795ae74647c63b92a40534792682939ea18057b2
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 21:44:39 +1000

    outlier is removed from nitrification and n.min

--- R/SummaryTableExcel.R
+++ R/SummaryTableExcel.R
@@ -1,3 +1,9 @@
+# remove outlier
+mine_RmOlr <- mine
+mine_RmOlr$nitrification[mine_RmOlr$nitrification == min(mine_RmOlr$nitrification)] <- NA
+mine_RmOlr$n.min[mine_RmOlr$n.min == min(mine_RmOlr$n.min)] <- NA
+mineMlt <- melt(mine_RmOlr, id = c("time", "date", "insertion", "sampling", "chamber", "location", "side", "temp"), na.rm= TRUE)
+
 # chamber summary table & mean
 ChSmmryTbl <- dlply(mineMlt, .(variable), function(x) CreateTable(x, fac = "chamber"))
 ChMean <- ddply(mineMlt, .(time, insertion, sampling, date, temp, chamber, variable), summarise, value = mean(value, na.rm = TRUE)) 
@@ -11,6 +17,9 @@ wb <- createWorkbook()
 # worksheet for rowdata and rowdata without outlier
 sheet <- createSheet(wb,sheetName="row_data")
 addDataFrame(mine, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
+
+sheet <- createSheet(wb,sheetName="row_data_withoutOutlier")
+addDataFrame(mine_RmOlr, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
 
 # worksheets for chamber summary
 shnames <- paste("Chamber_mean.",c("Nitrification", "N_mineralisation","P_mineralisation", sep=""))
