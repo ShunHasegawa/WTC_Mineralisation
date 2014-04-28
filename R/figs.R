31f615a0c6eae923645c859a192547e5123de40a
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 20:29:02 +1000

    modified ylabs to plot nutrient all at onece and also seprately

--- R/figs.R
+++ R/figs.R
@@ -1,4 +1,6 @@
-# summary data frame
+######################
+# summary data frame #
+######################
 ChMean <- ddply(mineMlt, .(time, date, insertion, sampling, temp, chamber, variable), Crt_SmryDF) 
 TrtMean <- TrtMean <- ddply(ChMean, .(time, insertion, sampling, date, temp, variable), 
                             function(x) Crt_SmryDF(x, val = "Mean"))
@@ -8,7 +10,9 @@ palette(c("blue2", "goldenrod1", "firebrick2", "chartreuse4", "deepskyblue1", "d
 
 theme_set(theme_bw()) # graphic backgroud is white
 
-# plot each nutrient separately
+#################################
+# plot each nutrient separately #
+#################################
 ChFg <- dlply(ChMean, .(variable), PltMean)
 fls <- paste("Output/Figs/WTC_Mineralisation_Chamber_", c("Nitrification", "N_mineralisation", "P_mineralisation"), ".pdf",sep = "")
 l_ply(1:3, function(x) ggsave(filename = fls[x], plot = ChFg[[x]], width = 6, height = 3))
@@ -16,3 +20,23 @@ l_ply(1:3, function(x) ggsave(filename = fls[x], plot = ChFg[[x]], width = 6, he
 TrtFg <- dlply(TrtMean, .(variable), PltMean)
 fls <- paste("Output/Figs/WTC_Mineralisation_Temp_", c("Nitrification", "N_mineralisation", "P_mineralisation"), ".pdf",sep = "")
 l_ply(1:3, function(x) ggsave(filename = fls[x], plot = TrtFg[[x]], width = 6, height = 3))
+
+##################################
+# plot all nutrient in one graph #
+##################################
+# labels for facet_grid
+ylabs <- list(
+  'nitrification' = "Nitrification rates",
+  'n.min' = "N mineralisation rates",
+  'p.min' = "P mineralisation rates")
+
+
+ylab_label <- function(variable, value){
+  return(ylabs[value])
+}
+
+pl <- PltMean(TrtMean) +
+  facet_grid(variable~., scales= "free_y", labeller= ylab_label)
+ggsave(filename = "Output//Figs/WTC_Mineralisation_Temp.pdf", plot = pl, width = 6, height = 6)
+
+
--- R/functions.R
+++ R/functions.R
@@ -145,9 +145,10 @@ PltMean <- function(data){
              expression(atop("N mineralisation rates", paste((dry_soil_mg^-1~day^-1)))),
              expression(atop("P mineralisation rates", paste((dry_soil_mg^-1~day^-1)))))
   
-  ylab <- ifelse(unique(data$variable) == "nitrification", ylabs[1], 
-                 ifelse(unique(data$variable) == "n.min", ylabs[2], 
-                        ylabs[3]))
+  ylab <- ifelse(length(unique(data$variable)) > 1, expression((dry_soil_mg^-1~day^-1)),
+                 ifelse(unique(data$variable) == "nitrification", ylabs[1], 
+                        ifelse(unique(data$variable) == "n.min", ylabs[2],
+                               ylabs[3])))
   
   colfactor <- ifelse(any(names(data) == "chamber"), "chamber", "temp")
   
