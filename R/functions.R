e047a38a4a827cc06267378db67c26ebfe53ab1d
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 20:42:49 +1000

    functions to be used for stat analysis are copied from WTC_IEM

--- R/functions.R
+++ R/functions.R
@@ -165,3 +165,51 @@ PltMean <- function(data){
     guides(color = guide_legend(keyheight = 0.7))
 }
 
+#######################
+#model simplification #
+#######################
+MdlSmpl <- function(model){
+  mod2 <- update(model, method = "ML") #change method from REML to ML
+  stai <- stepAIC(mod2, trace = FALSE) #model simplification by AIC
+  dr <- drop1(stai, test="Chisq") #test if removing a factor even more significantly lowers model
+  model <- update(stai, method="REML")
+  ifelse(all(dr[[4]] < 0.05, na.rm=TRUE), anr <- anova(model), anr<-NA) 
+  #dr[[4]]<0.05-->unable to remove any more factors so finlize the results by changsing the method back to REML
+  return(list(step.aic = stai$anova, drop1 = dr, anova.reml = anr, model.reml = model, model.ml = stai))
+}
+
+#############################################
+# compare different auto-correlation models #
+#############################################
+atcr.cmpr <- function(model, rndmFac){
+  if(rndmFac == "Chamber/Location"){
+    model2 <- update(model,corr=corCompSymm(form=~1|Chamber/Location)) 
+  } else {
+    if(rndmFac == "Chamber"){
+      model2 <- update(model,corr=corCompSymm(form=~1|Chamber))
+    } else {
+      model2 <- update(model,corr=corCompSymm(form=~1|id))
+    }
+  }
+  
+  model3 <- update(model,correlation=corARMA(q=2))
+  model4 <- update(model,correlation=corAR1()) 
+  model5 <- update(model,correlation=corARMA(q=1))
+  a <- anova(model,model2,model3,model4,model5)
+  models <- list(model, model2, model3, model4, model5, a)
+  return(models)
+}
+
+###########################################
+# produce box plots with transformed data #
+###########################################
+# log OR sqrt OR power(1/3)
+bxplts <- function(value, ofst = 0, data){
+  par(mfrow = c(2,2))
+  y <- data[[value]] + ofst #ofst is added to make y >0
+  boxplot(y ~ temp*Time, data)
+  boxplot(log(y) ~ temp*Time, main = "log", data)
+  boxplot(sqrt(y) ~ temp*Time, main = "sqrt", data)
+  boxplot(y^(1/3) ~ temp*Time, main = "power(1/3)", data)
+  par(mfrow = c(1,1))
+}
