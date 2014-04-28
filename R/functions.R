ed801ad0fbfd50f6c5d75c855a489910f66d2594
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 21:02:25 +1000

    updated

--- R/functions.R
+++ R/functions.R
@@ -183,7 +183,7 @@ MdlSmpl <- function(model){
 #############################################
 atcr.cmpr <- function(model, rndmFac){
   if(rndmFac == "chamber/side"){
-    model2 <- update(model,corr=corCompSymm(form=~1|Chamber/side)) 
+    model2 <- update(model,corr=corCompSymm(form=~1|chamber/side)) 
   } else {
     if(rndmFac == "chamber"){
       model2 <- update(model,corr=corCompSymm(form=~1|Chamber))
