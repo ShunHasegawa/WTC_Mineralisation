3c954e4d8e860f5f8a88d127aa685e6e5a5b60c5
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-28 16:46:13 +1000

    save summary table as excel

--- R/Analysis.R
+++ R/Analysis.R
@@ -43,4 +43,20 @@ ChMean <- ddply(mineMlt, .(time, insertion, sampling, date, temp, chamber, varia
 # treat summary table $ mean
 TrtSmmryTbl <- dlply(ChMean, .(variable), function(x) CreateTable(x, fac = "temp"))
 
+## create xcel workbook ##
+wb <- createWorkbook()
 
+# worksheet for rowdata and rowdata without outlier
+sheet <- createSheet(wb,sheetName="row_data")
+addDataFrame(mine, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
+
+# worksheets for chamber summary
+shnames <- paste("Chamber_mean.",c("Nitrification", "N_mineralisation","P_mineralisation", sep=""))
+l_ply(1:3, function(x) crSheet(sheetname = shnames[x], dataset = ChSmmryTbl[[x]]))
+
+# worksheets for temp trt summary
+shnames <- paste("Temp_mean.", c("Nitrification", "N_mineralisation","P_mineralisation"), sep = "")
+l_ply(1:3, function(x) crSheet(sheetname = shnames[x], dataset = TrtSmmryTbl[[x]]))
+
+#save file
+saveWorkbook(wb,"Output/Table/WTC_Mineralisation.xlsx")
