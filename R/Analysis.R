b5dca8919020cc783aedc3803a4e8dfbcf89ae00
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-28 16:42:27 +0000

    reanalysed with lmer

--- R/Analysis.R
+++ R/Analysis.R
@@ -7,16 +7,17 @@ source("R/functions.R")
 # Mineralisation #
 ##################
 # process data
-mine <- read.csv("Data/WTC_mineralisation.csv", colClasses=c("time" = "factor", "location" = "factor"))
-mine <- mine[complete.cases(mine),]
-mine <- droplevels(mine)
-mine$chamber <- factor(ifelse(mine$chamber < 10, paste("0", mine$chamber, sep = ""), mine$chamber))
-mine$insertion <- as.Date(dmy(mine$insertion))
-mine$sampling <- as.Date(dmy(mine$sampling))
-mine$date <- as.Date(ave(apply(cbind(mine$insertion, mine$sampling), 1, mean), mine$time), 
-                     origin = origin) # same date for same date
-mine$id <- mine$chamber:mine$side
-save(mine, file = "Output//Data/WTC_Mineralisation.RData")
+# mine <- read.csv("Data/WTC_mineralisation.csv", colClasses=c("time" = "factor", "location" = "factor"))
+# mine <- mine[complete.cases(mine),]
+# mine <- droplevels(mine)
+# mine$chamber <- factor(ifelse(mine$chamber < 10, paste("0", mine$chamber, sep = ""), mine$chamber))
+# mine$insertion <- as.Date(dmy(mine$insertion))
+# mine$sampling <- as.Date(dmy(mine$sampling))
+# mine$date <- as.Date(ave(apply(cbind(mine$insertion, mine$sampling), 1, mean), mine$time), 
+#                      origin = origin) # same date for same date
+# mine$id <- mine$chamber:mine$side
+# save(mine, file = "Output//Data/WTC_Mineralisation.RData")
+load("Output//Data/WTC_Mineralisation.RData")
 
 #################
 # Summary table #
--- R/stats_Pmineralisation.R
+++ R/stats_Pmineralisation.R
@@ -3,52 +3,35 @@
 range(mine$p.min)
 bxplts(value = "p.min", ofst= 0.06, data = mine)
 bxcxplts(value = "p.min", data = mine, sval = 0.06, fval = .1)
-
 # log seems better
 
-# different random factor structure
-m1 <- lme(log(p.min + .06) ~ temp * time, random = ~1|chamber/side, data = mine)
-m2 <- lme(log(p.min + .06) ~ temp * time, random = ~1|chamber, data = mine)
-m3 <- lme(log(p.min + .06) ~ temp * time, random = ~1|id, data = mine)
-anova(m1, m2, m3)
-# m3 or (m2) is slightly better
-
-# autocorrelation
-atcr.cmpr(m3, rndmFac= "id")$models
-# no need for auto-correlation
-
-Iml <- atcr.cmpr(m3, rndmFac= "id")[[1]]
-
-# The initial model is:
-Iml$call
+# The initial model is
+Iml <- lmer(log(p.min + .06) ~ temp * time + (1|chamber) + (1|id), data = mine)
 Anova(Iml)
 
-# model simplification
-MdlSmpl(Iml)
-# interaction of temp x time is removed
-
-Fml <- MdlSmpl(Iml)$model.reml
-
-# The final model is:
-Fml$call
-
+# The final model is
+Fml <- stepLmer(Iml)
 Anova(Fml)
+AnvF_P <- Anova(Fml, test.statistic = "F")
+AnvF_P
 
 summary(Fml)
 
+plot(allEffects(Fml))
+
 # model diagnosis
 plot(Fml)
-qqnorm(Fml, ~ resid(.)|id)
-qqnorm(residuals.lm(Fml))
-qqline(residuals.lm(Fml))
+qqnorm(resid(Fml))
+qqline(resid(Fml))
 
 ## ----Stat_WTC_Mine_Pmineralisation_Smmry
 # The initial model is:
-Iml$call
+Iml@call
 
 Anova(Iml)
 
 # The final model is:
-Fml$call
+Fml@call
 
 Anova(Fml)
+AnvF_P
